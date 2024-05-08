a688f92334a506615851a70c40283b9b4be3f573
Yash KB <yashkb79@gmail.com>
2024-05-08 19:18:32 +0530

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Design_Pattern_Examples
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
